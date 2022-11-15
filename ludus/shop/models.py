a837fd70300a25ccad35b4339b259ed978ec3b05
Demon2rus <sunbro20001@gmail.com>
2022-11-15 22:18:23 +1000

    Update models.py

--- ludus/shop/models.py
+++ ludus/shop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Product(models.Model): #Товар
     title = models.CharField(max_length=255)
-    content = models.TextField(blank = True)
+    description = models.TextField(blank = True)
     photo = models.ImageField(upload_to = "photos/%Y/%m/%d/")
     price = models.PositiveIntegerField()
     time_create = models.DateTimeField(auto_now_add=True)
